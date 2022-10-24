894b31e17bdc78dd735d01572d69fbe7c321ad0c
OreyM <oreymgt@gmail.com>
2022-10-24 17:33:41 +0300

    Auth registration

---
+++ src/Controller/Auth/RegistrationController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Controller\Auth;
+
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use App\Repository\UserRepository;
+use App\Security\LoginFormAuthenticator;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
+
+class RegistrationController extends AbstractController
+{
+    #[Route(path: '/registration', name: 'app_registration')]
+    public function __invoke(): Response
+    {
+        $form = $this->createForm(RegistrationFormType::class, new User(), [
+            'action' => $this->generateUrl('app_registration_register'),
+            'method' => 'POST',
+        ]);
+
+        $params['form'] = $form->createView();
+
+        return $this->render('security/registration.html.twig', $params);
+    }
+
+    /**
+     * @param Request $request
+     * @param UserRepository $repository
+     * @param UserPasswordHasherInterface $hasher
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    #[Route(path: '/registration/register', methods: 'post', name: 'app_registration_register')]
+    public function register(
+        Request $request,
+        UserRepository $repository,
+        UserPasswordHasherInterface $hasher,
+        UserAuthenticatorInterface $authenticator,
+        LoginFormAuthenticator $formAuthenticator
+    ): RedirectResponse
+    {
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user
+                ->setRoles([User::ROLE_USER])
+                ->setPassword($hasher->hashPassword($user, $user->getPassword()))
+                ->setCreatedAt()
+                ->setUpdatedAt();
+
+            $repository->save($user, true);
+
+            return $authenticator->authenticateUser(
+                $user,
+                $formAuthenticator,
+                $request);
+        }
+
+        return $this->redirectToRoute('app_registration');
+    }
+}
---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'required' => true,
+                'constraints' => [
+                    new Email(),
+                    new Length(null, 3, 255),
+                    // ToDo valid unique email
+                ],
+                'attr'  => [
+                    'class'         => 'form-control-lg',
+                    'placeholder'   => 'Input email',
+                ],
+            ])
+            ->add('name', TextType::class, [
+                'label' => 'Name',
+                'required' => true,
+                'constraints' => [
+                    new Length(null, 3, 255),
+                ],
+                'attr'  => [
+                    'class'         => 'form-control-lg',
+                    'placeholder'   => 'Input Your name',
+                ],
+            ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'first_options'     => [
+                    'label' => 'Password',
+                    'constraints' => [
+                        new Length(null, 8, 255),
+                    ],
+                    'attr'  => [
+                        'class'         => 'form-control-lg',
+                        'placeholder'   => 'Input password',
+                    ],
+                ],
+                'second_options'    => [
+                    'label' => 'Confirm Password',
+                    'attr'  => [
+                        'class'         => 'form-control-lg',
+                        'placeholder'   => 'Confirm password',
+                    ],
+                ],
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Register',
+                'attr'  => [
+                    'class' => 'btn-outline-light btn-lg px-5',
+                ],
+            ]);;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+
+    /**
+     * @return string
+     */
+    public function getBlockPrefix(): string
+    {
+        return '';
+    }
+}
