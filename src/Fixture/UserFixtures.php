79157bb347a63e0c9851737636d0e7f335342dba
OreyM <oreymgt@gmail.com>
2022-10-26 12:47:16 +0300

    add users fixtures

---
+++ src/Fixture/UserFixtures.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Fixture;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory as Faker;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    private UserRepository $repository;
+    private UserPasswordHasherInterface $hasher;
+    private $faker;
+
+    public function __construct(
+        UserRepository $repository,
+        UserPasswordHasherInterface $hasher,
+        Faker $faker
+    )
+    {
+        $this->repository = $repository;
+        $this->hasher = $hasher;
+        $this->faker = $faker::create();
+    }
+
+    /**
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager): void
+    {
+        for ($i = 0; $i < 10; ++$i) {
+            $user = new User();
+            $user->setEmail($this->faker->email())
+                ->setName($this->faker->name())
+                ->setRoles([User::ROLE_USER])
+                ->setPassword(
+                    '$2y$13$pruD8xbAtwwZf6FHHW.I6eAxp01TSZxUGE4hiAYzbaj7NgI088RWW'
+                )
+                ->setCreatedAt()
+                ->setUpdatedAt();
+
+            $this->repository->save($user, true);
+        }
+    }
+}
